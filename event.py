c7b99d89a4c134d4dd7cf6f32a98ae4e673d7694
Adrian Duran <adrianmdu@gmail.com>
2015-10-19 01:35:58 -0300

    Format code.

--- event.py
+++ event.py
@@ -14,11 +14,9 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 DEFAULT_EVENT_NAME = 'event_group'
 
-
 def event_key(event_name=DEFAULT_EVENT_NAME):
     # Constructs a Datastore key for a Event entity.
     return ndb.Key('Events', event_name)
-
 
 class Event(ndb.Model):
     identity = ndb.StringProperty(indexed=True)
@@ -27,19 +25,16 @@ class Event(ndb.Model):
     total = ndb.IntegerProperty(indexed=False)
     creationDate = ndb.DateTimeProperty(auto_now_add=True)
 
-
 class User(ndb.Model):
     email = ndb.StringProperty(indexed=True)
     name = ndb.StringProperty(indexed=False)
     company = ndb.StringProperty(indexed=False)
-
 
 class EventPage(webapp2.RequestHandler):
     def get(self):
         event_id = self.request.get('eventId')
         event_query = Event.query(Event.identity == event_id, ancestor=event_key())
         event = event_query.get()
-
         if (event):
             template_values = {
                 'event': event,
@@ -60,29 +55,20 @@ class EventPage(webapp2.RequestHandler):
             if event.available > 0:
                 eventKey = ndb.Key("Events", event_key().id(), "Event", event.key.id())
                 user = User(parent=eventKey)
-
-                jsonstring = self.request.body
-                jsonobject = json.loads(jsonstring)
-
+                jsonobject = json.loads(self.request.body)
                 user.email = jsonobject.get('userEmail')
                 user.name = jsonobject.get('userName')
                 user.company = jsonobject.get('userCompany')
-
                 user.put()
-
                 event.available -= 1
                 event.put()
-
                 self.response.out.write(json.dumps({'status': 'success'}))
             else:
-                self.response.out.write(json.dumps({'status': 'full event'}))
-
-
+                self.response.out.write(json.dumps({'status': 'event full'}))
         else:
             self.response.status_int = 404
             self.response.status = "404 - Not found"
             self.response.body = "The event can not be found. Check your url (eventId) or contact your system administrator."
-
 
     def delete(self):
         event_id = self.request.get('eventId')
@@ -96,9 +82,7 @@ class EventStatus(webapp2.RequestHandler):
         event_id = self.request.get('eventId')
         event_query = Event.query(Event.identity == event_id, ancestor=event_key())
         event = event_query.get()
-
         eventKey = ndb.Key("Events", event_key().id(), "Event", event.key.id())
-
         user_query = User.query(ancestor=eventKey)
         users = user_query.fetch()
 
@@ -114,13 +98,10 @@ class EventStatus(webapp2.RequestHandler):
             self.response.status = "404 - Not found"
             self.response.body = "The event can not be found. Check your url (eventId) or contact your system administrator."
 
-
 class UserStatus(webapp2.RequestHandler):
     def get(self):
-
         event_id = self.request.get('eventId')
         user_email = self.request.get('userId')
-
         event_query = Event.query(Event.identity == event_id, ancestor=event_key())
         event = event_query.get()
 
@@ -129,12 +110,10 @@ class UserStatus(webapp2.RequestHandler):
             user_query = User.query(User.email == user_email, ancestor=eventKey)
             user = user_query.get()
             self.response.headers['Content-Type'] = "application/json"
-
             if user:
                 self.response.out.write(json.dumps({'confirmed': 'true'}))
             else:
                 self.response.out.write(json.dumps({'confirmed': 'false'}))
-
         else:
             self.response.status_int = 404
             self.response.status = "404 - Not found"
@@ -164,11 +143,10 @@ class Admin(webapp2.RequestHandler):
 
         self.redirect('/create')
 
-
 app = webapp2.WSGIApplication([
                                   ('/event', EventPage),
                                   ('/userStatus', UserStatus),
                                   ('/eventStatus', EventStatus),
-                                  ('/', Admin),
                                   ('/create', Admin),
+                                  ('/', Admin),
                               ], debug=True)
